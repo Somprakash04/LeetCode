69f15a9822c0dc5b070a23d305b59f904dff3490
Sparun19 <somprakasharun@gmail.com>
2025-07-05 14:02:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (86.32%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> searchRange(vector<int>& nums, int tar) {
         vector<int>ans(2,-1);
-        int n = nums.size();
-
-        // First occurrence
-        int st = 0, end = n - 1;
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-            if (nums[mid] < target) {
-                st = mid + 1;
-            } else {
-                end = mid - 1;
+        int  n= nums.size();
+        int st = 0 , end = n-1;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(nums[mid] ==  tar) ans[0] = mid;
+            if(nums[mid]<tar){
+                st = mid+1;
             }
-            if (nums[mid] == target) ans[0] = mid;
-        }
-
-        // Last occurrence
-        st = 0, end = n - 1;
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-            if (nums[mid] > target) {
-                end = mid - 1;
-            } else {
-                st = mid + 1;
+            else{
+                end = mid-1;
             }
-            if (nums[mid] == target) ans[1] = mid;
         }
-
+         st = 0, end = n-1;
+         while(st<=end){
+            int mid = st+(end-st)/2;
+            if(nums[mid] ==  tar) ans[1] = mid;
+            if(nums[mid]>tar){
+                end = mid-1;
+            }
+            else{
+                st = mid+1;
+            }
+         }
         return ans;
     }
 };
