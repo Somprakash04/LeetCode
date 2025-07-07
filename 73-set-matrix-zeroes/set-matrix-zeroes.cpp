735bc0b5ab68b3761978e50c3436a6c16f673974
Sparun19 <somprakasharun@gmail.com>
2025-07-07 18:56:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,6 +3,8 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+        // T.C. & S.C.= O(n*m)
+        /*
         set<int> rows, cols;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
@@ -22,5 +24,26 @@ public:
                 matrix[i][col]=0;
             }
         }
+        */
+        // T.C. = O(n*m)
+        // S.C. = O(n+m)
+        vector<bool>rows(n, false);
+        vector<bool>cols(m, false);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(rows[i] || cols[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
     }
 };
