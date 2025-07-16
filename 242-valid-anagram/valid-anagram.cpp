504efb0539562f844ea4f8c7bfcaf5c03f1d8297
Sparun19 <somprakasharun@gmail.com>
2025-07-16 19:38:05 +0530

    Time: 3 ms (51.77%) | Memory: 9.6 MB (74.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        /* --Brute Force approach--
         if( s.size() != t.size()) return false;
-
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
+        return s == t;*/
+        
+        //Better approach--
+        if( s.size() != t.size()) return false;
+        vector<int>count(26,0);
 
-        return s == t;
+        for(char c : s) count[c-'a']++;
+        for(char c : t) count[c-'a']--;
+
+        for(int freq : count){
+            if(freq != 0) return false; 
+        }
+        return true;
     }
 };
